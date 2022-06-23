e710de15d9aa9177d73bb2d900d2b1afd72020b3
Jegor Zelenjak <jegorkin@mail.ru>
2022-06-23 14:26:45 +0200

    Add lock.sh
    For some reason, i cannot change the keyboard layout in xsecurelock.
    Since i have to shut down my computer if i happened to have a
    non-English keyboard layout when locking the screen, i want to switch to
    the English layout to avoid the problem.

---
+++ lock.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# A mini script to avoid locking screen with non-English keyboard, since for some reason i cannot change the keyboard layout in xsecurelock
+
+setxkbmap us; xsecurelock ; setxkbmap us,ru,ee
