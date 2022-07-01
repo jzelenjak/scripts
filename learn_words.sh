a2984d7f84b4554aa9a39a948eae7a6bc23e567b
Jegor Zelenjak <jegorkin@mail.ru>
2022-07-01 11:46:18 +0200

    Update learn_words.sh
    Improve randomization by making the loop pass over the entire file (in a
    shuffled order) first, after which it creates another permutation of the
    input file and loops over it again. In addition, the urgency is now set
    to critical so that the user can control the time before the answer
    appears. Finally, an icon has been added to the popup.

--- learn_words.sh
+++ learn_words.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# Gets a random word + translation from a .csv file and creates a notification with the word and its translation
-# This script can be used as a background process to help with learning a foreign language
-# When --active-recall option is specified, the translation is displayed first, the user has 5 seconds to recall the word, and then the answer is displayed.
+# Gets a word + translation from a random permutation of a.csv file and creates a notification with the word and its translation.
+# This script can be used as a background process to help with learning a foreign language.
+# When --active-recall option is specified, the translation is displayed first, and after the popup is closed, the answer is displayed.
 
 # Checks if the file with the words has been specified
 if [[ $# -eq 0 || ! -f $1 ]]; then
     echo "$0: could not find the file with the words"
+    exit 2
 fi
 
 # Gets the interval in minutes (default: 10 minutes)
@@ -20,18 +21,19 @@ if [[ "$3" = "--active-recall" ]]; then
 fi
 
 # Start the infinite loop until the process is not interrupted or killed
-echo You will get a new word every $((interval / 60)) $(test $interval -eq 60 && echo minute || echo minutes)
+echo "You will get a new word every $((interval / 60)) $(test $interval -eq 60 && echo minute || echo minutes)"
 
 while [[ "Jegor" != "smart" ]]; do
-    IFS=',' read word translation <<< $(shuf -n 1 $1)
+    while read pair; do
+        IFS=',' read word translation <<< $pair
 
-    if [[ $recall ]]; then
-        notify-send "How do you say" "$translation"
-        sleep 5
-        notify-send "Answer" "$word"
-    else
-        notify-send "$word" "$translation"
-    fi
+        if [[ $recall ]]; then
+            notify-send -i accessories-dictionary-symbolic -u critical "How do you say" "$translation"  # critical urgency is to make sure that the popup does not disappear
+            notify-send -i accessories-dictionary-symbolic -u critical "Answer" "$word"                 # if `accessories-dictionaries-symbolic` is not found, remove this option
+        else
+            notify-send -i accessories-dictionary-symbolic -u critical "$word" "$translation"
+        fi
 
-    sleep $interval
+        sleep $interval
+    done < <(shuf $1)
 done
