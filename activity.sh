d98656a66bf1ef7206150b4b6548373ba7523589
Jegor Zelenjak <jegorkin@mail.ru>
2022-11-14 15:22:33 +0100

    Change avg_commit_time.sh to use awk instead of the while loop bloat

--- activity.sh
+++ activity.sh
@@ -1,31 +1,47 @@
 #!/bin/bash
 # Computes the activity over days
+# By default, the output is sorted by the date (chronologically)
+# If the option -s is specified, the available option arguments are:
+#  1 = sort by the number of insertions
+#  2 = sort by the number of deletions
+
+
+set -euo pipefail
+IFS=$'\n\t'
+
+umask 077
+
+usage="usage: $0 [-s 1 | -s 2]"
 
 
 # Check if the script is run in a git repository
-git status 2&> /dev/null
-exit_code=$?
 
-if [ $exit_code -ne 0 ]; then
-    echo Not a git repository
-    exit $exit_code
-fi
+git status &> /dev/null || { echo "not a git repository" >&2 ; exit 2; }
 
 
 # Check if an invalid option is provided
-if [[ $# -ne 0 ]] && [[ "$1" != "-s" ]]; then
-    echo Unrecognized option $1
+sorting=${1:-}
+if [[ -n "$sorting" ]] && [[ "$sorting" != "-s" ]]; then
+    echo "unknown option: $1" >&2
+    echo "$usage" >&2
     exit 1
 fi
 
 # Check if the user requested sorting by the number of insertions or deletions
 order_by=1
-if [ "$1" == "-s" ]; then
-    if [[ "$2" == "" ]] || [[ $2 -ne 1 ]] && [[ $2 -ne 2 ]] ; then
-        echo "Please give a valid option for the option -s (1 = sort by the number of insertions, 2 = sort by the number of deletions)"
-        exit
+sort_options=""
+if [ "$sorting" ]; then
+    sort_order=${2:-}
+    [[ -z "$sort_order" ]] && { echo -e "missing option argument for option -s\n$usage" >&2 ; exit 1; }
+
+    if [[ "$sort_order" -ne 1 ]] && [[ "$sort_order" -ne 2 ]] ; then
+        echo "unrecognized option argument for option -s: $sort_order" >&2
+        echo "$usage" >&2
+        exit 1
     fi
-    order_by="$(($2 + 1)) -n -r"
+
+    order_by="$(($sort_order + 1))"
+    sort_options="-nr"
 fi
 
 # Compute the activity (#insertions,#deletions) over days and sort as needed
@@ -37,7 +53,8 @@ git log --no-merges --pretty=format:'%as' --shortstat |  # Get the date of a com
     sed -r 's/^.* changed, ([0-9]+).*, ([0-9]+).*$/,\1,\2\./' |  # Extract the number of insertions and deletions and separate them with a comma. A dot will be needed in the next steps
     tr -d '\n' |  # Delete \n to merge the line with the date of a commit with the next line with the changes of this commit (side effect: everything is one huge line now. But a dot is a separator between commits)
     tr '.' '\n' |  # Since a dot acts as a separator between commits, make the separator be \n so that one commit corresponds to one line
-    sort -t"," -k 1 |  # Make same dates appear next to each other
+    sort -t "," -k 1 |  # Make same dates appear next to each other
     awk -F "," '{ insertions[$1] += $2; deletions[$1] += $3 } END { for (i in insertions) { print i "," insertions[i] "," deletions[i] } }'  | # Basically, what in SQL would be GROUP BY and SUM
-    sort  -t"," -k $order_by |  # Sort as requested (by date, by the number of insertions, by the number of deletions)
+    sort  -t "," -k "$order_by" "$sort_options" |  # Sort as requested (by date, by the number of insertions, by the number of deletions)
     tr ',' '\t'  # Make the separator to be a tab (easier to read)
+
--- avg_commit_time.sh
+++ avg_commit_time.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
-# Shows the average commit time (the average time a commit has been made)
+# Shows the average commit time (the average time a commit has been made) inside a git repository
 
-# Check if the script is running inside a git repository
-git status 2&> /dev/null
-exit_code=$?
+set -euo pipefail
+IFS=$'\n\t'
 
-if [ $exit_code -ne 0 ]; then
-    echo "Not a git repository"
-    exit $exit_code
-fi
+umask 077
+
+
+# Check if the script is run in a git repository
+git status &> /dev/null || { echo "not a git repository" >&2 ; exit 2; }
+
 
 # Compute the average time
-sum=0
-total=0
+git log --date=format:'%H %M %S' --pretty=format:%ad |
+    awk '
+    {
+        sum += ($3 + $2 * 60 + $1 * 3600)
+        total += 1
+    }
+    END {
+        avg_time_seconds = sum / total
+        hours = int(avg_time_seconds / 3600)
+        minutes = int(avg_time_seconds % 3600 / 60)
+        print "Total commits found:", total
+        print "The average time of a commit is " hours ":" minutes
+    }
+    '
 
-while read time; do
-    read hours minutes seconds <<< $time
-    time_seconds=$(expr $seconds + $minutes \* 60 + $hours \* 3600)
-    sum=$(($sum + $time_seconds))
-    total=$(($total + 1))  # doing it with `wc -l` is better, but i don't want to run the pipeline twice or store the results in a temp file
-done <<<$(git log --date=format:'%H %M %S' --pretty=format:%ad)
-
-avg_time_seconds=$(($sum / $total))
-
-hours=$((avg_time_seconds / 3600))
-minutes=$((avg_time_seconds % 3600 / 60))
-
-echo "Total commits found: $total"
-echo The average time of a commit is $hours:$minutes
